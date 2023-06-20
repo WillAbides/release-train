ae39e35fa24814d5462fcc3e62b962201cfbe535
Will Roden <will@roden.cc>
2023-06-20 11:52:15 -0500

    remove prev command

--- internal/cmd/release-train/root.go
+++ internal/cmd/release-train/root.go
@@ -8,7 +8,6 @@ type rootCmd struct {
 	CheckoutDir string            `kong:"short=C,default='.',help=${checkout_dir_help}"`
 	Label       map[string]string `kong:"help=${label_help}"`
 	Release     releaseCmd        `kong:"cmd,help='Release a new version.'"`
-	Prev        prevCmd           `kong:"cmd,help='Get the previous version.'"`
 	Action      actionCmd         `kong:"cmd"`
 	Version     kong.VersionFlag
 }
