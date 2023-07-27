e9dbaf7e9d58f6d0d24529c852795cb9ba52dd44
Will Roden <will@roden.cc>
2023-07-27 11:27:01 -0400

    no more vars.yaml

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	_ "embed"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -16,18 +15,70 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-//go:embed vars.yaml
-var varsYaml []byte
-
 var version = "dev"
+
+var flagHelp = kong.Vars{
+	"generate_action_help": `Ignore all other flags and generate a GitHub action.`,
+	"ref_help":             `git ref.`,
+	"checkout_dir_help":    `The directory where the repository is checked out.`,
+	"create_tag_help":      `Whether to create a tag for the release.`,
+	"create_release_help":  `Whether to create a release. Implies create-tag.`,
+	"initial_tag_help":     `The tag to use if no previous version can be found. Set to "" to cause an error instead.`,
+	"tag_prefix_help":      `The prefix to use for the tag.`,
+	"label_help":           `PR label alias in the form of "<alias>=<label>" where <label> is a canonical label.`,
+	"output_format_help":   `Output either json our GitHub action output.`,
+	"debug_help":           `Enable debug logging.`,
+	"draft_help":           `Leave the release as a draft.`,
+	"tempdir_help":         `The prefix to use with mktemp to create a temporary directory.`,
+	"pushremote_help":      `The remote to push tags to.`,
+	"repo_help":            `Github repository in the form of owner/repo.`,
+	"github_token_help":    "The GitHub token to use for authentication. Must have `contents: write` permission if creating a release or tag.",
+	"github_api_url_help":  `GitHub API URL.`,
+
+	"check_pr_help": `
+Operates as if the given PR has already been merged. Useful for making sure the PR is properly labeled.
+Skips tag and release.
+`,
+
+	"pre_release_hook_help": `
+Command to run before creating the release. You may abort the release by exiting with a non-zero exit code.
+  
+Exit code 0 will continue the release. Exit code 10 will skip the release without error. Any other exit code will
+abort the release with an error.
+
+You may provide custom release notes by writing to the file at $RELEASE_NOTES_FILE:
+
+    echo "my release notes" > "$RELEASE_NOTES_FILE"
+
+You can update the git ref to be released by writing it to the file at $RELEASE_TARGET:
+
+    # ... update some files ...
+    git commit -am "prepare release $RELEASE_TAG"
+    echo "$(git rev-parse HEAD)" > "$RELEASE_TARGET"
+
+If you create a tag named $RELEASE_TAG, it will be used as the release target instead of either HEAD or the value
+written to $RELEASE_TARGET.
+
+Any files written to $ASSETS_DIR will be uploaded as release assets.
+
+The environment variables RELEASE_VERSION, RELEASE_TAG, PREVIOUS_VERSION, FIRST_RELEASE, GITHUB_TOKEN,
+RELEASE_NOTES_FILE, RELEASE_TARGET and ASSETS_DIR will be set.
+`,
+
+	"v0_help": `
+Assert that current major version is 0 and treat breaking changes as minor changes.
+Errors if the major version is not 0.
+`,
+
+	"release_ref_help": `
+Only allow tags and releases to be created from matching refs. Refs can be patterns accepted by git-show-ref.
+If undefined, any branch can be used.
+`,
+}
 
 func main() {
 	ctx := context.Background()
-	vars := kong.Vars{}
-	err := yaml.Unmarshal(varsYaml, &vars)
-	if err != nil {
-		panic(err)
-	}
+	vars := flagHelp
 	vars["version"] = version
 
 	var root rootCmd
@@ -54,12 +105,12 @@ type rootCmd struct {
 	Draft          bool              `help:"${draft_help}"`
 	TagPrefix      string            `default:"v" help:"${tag_prefix_help}"`
 	V0             bool              `name:"v0" help:"${v0_help}"`
-	InitialTag     string            `action:"initial-release-tag" help:"${initial_tag_help}" default:"${initial_tag_default}"`
+	InitialTag     string            `action:"initial-release-tag" help:"${initial_tag_help}" default:"v0.0.0"`
 	PreReleaseHook string            `placeholder:"<command>" help:"${pre_release_hook_help}"`
 	ReleaseRef     []string          `action:"release-refs" placeholder:"<branch>" help:"${release_ref_help}"`
 	PushRemote     string            `action:"-" default:"origin" help:"${pushremote_help}"`
 	Tempdir        string            `help:"${tempdir_help}"`
-	GithubApiUrl   string            `action:"-" help:"${github_api_url_help}" default:"${github_api_url_default}"`
+	GithubApiUrl   string            `action:"-" help:"${github_api_url_help}" default:"https://api.github.com"`
 	OutputFormat   string            `action:"-" default:"json" help:"${output_format_help}" enum:"json,action"`
 	Debug          bool              `help:"${debug_help}"`
 }
