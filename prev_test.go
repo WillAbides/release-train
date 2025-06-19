2e51907249e3ae21ed6aa69764ae32206181c940
Will Roden <will@roden.cc>
2025-06-19 15:39:36 -0500

    refactor Test_getPrevTag

--- prev_test.go
+++ prev_test.go
@@ -1,60 +1,75 @@
 package main
 
 import (
-	"os"
+	"cmp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func Test_getPrevTag(t *testing.T) {
-	ctx := t.Context()
-	dir := t.TempDir()
-	_, e := runCmd(ctx, &runCmdOpts{
-		dir: dir,
-		env: map[string]string{
-			"GIT_AUTHOR_NAME":    "foo",
-			"GIT_COMMITTER_NAME": "foo",
-			"EMAIL":              "foo@example.com",
+	const stdSetup = `
+		git init
+		git commit --allow-empty -m "first"
+		git tag v0.1.0
+		git tag foo0.1.0
+		git tag v0.1.1
+		git tag foo0.1.1
+		git tag v0.2.0
+		git tag foo0.2.0
+		git tag v1.0.0
+		git commit --allow-empty -m "second"
+		git commit --allow-empty -m "third"
+		git tag v2.0.0
+		git tag foo
+		git commit --allow-empty -m "forth"
+		git tag bar
+	`
+
+	stdSetupEnv := map[string]string{
+		"GIT_AUTHOR_NAME":    "foo",
+		"GIT_COMMITTER_NAME": "foo",
+		"EMAIL":              "foo@example.com",
+	}
+
+	for _, test := range []struct {
+		name     string
+		setupCmd string
+		setupEnv map[string]string
+		opts     getPrevTagOpts
+		wantTag  string
+		wantErr  bool
+	}{
+		{
+			name:     "",
+			setupCmd: stdSetup,
+			opts:     getPrevTagOpts{TagPrefix: "v"},
+			wantTag:  "v2.0.0",
 		},
-		stdout: os.Stdout,
-		stderr: os.Stderr,
-	}, "sh", "-c", `
-git init
-git commit --allow-empty -m "first"
-git tag v0.1.0
-git tag foo0.1.0
-git tag v0.1.1
-git tag foo0.1.1
-git tag v0.2.0
-git tag foo0.2.0
-git tag v1.0.0
-git commit --allow-empty -m "second"
-git commit --allow-empty -m "third"
-git tag v2.0.0
-git tag foo
-git commit --allow-empty -m "forth"
-git tag bar
-`)
-	require.NoError(t, e)
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := t.Context()
+			dir := t.TempDir()
+			setupOpts := &runCmdOpts{
+				dir: dir,
+				env: test.setupEnv,
+			}
+			setupCmd := cmp.Or(test.setupCmd, stdSetup)
+			if setupOpts.env == nil {
+				setupOpts.env = stdSetupEnv
+			}
+			_, err := runCmd(ctx, setupOpts, "sh", "-c", setupCmd)
+			require.NoError(t, err)
 
-	t.Run("", func(t *testing.T) {
-		opts := getPrevTagOpts{
-			RepoDir:   dir,
-			TagPrefix: "v",
-		}
-		got, err := getPrevTag(ctx, &opts)
-		require.NoError(t, err)
-		require.Equal(t, "v2.0.0", got)
-	})
-
-	t.Run("", func(t *testing.T) {
-		opts := getPrevTagOpts{
-			RepoDir:   dir,
-			TagPrefix: "v",
-		}
-		got, err := getPrevTag(ctx, &opts)
-		require.NoError(t, err)
-		require.Equal(t, "v2.0.0", got)
-	})
+			opts := test.opts
+			opts.RepoDir = cmp.Or(opts.RepoDir, dir)
+			got, err := getPrevTag(ctx, &opts)
+			if test.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, test.wantTag, got)
+		})
+	}
 }
